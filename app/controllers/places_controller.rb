096bd5d3846bebd1cab64d846d89a8bd0950a031
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-10-05 14:59:43 +0900

    温度のグラフを追加(横軸がおかしい)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,6 +10,19 @@ class PlacesController < ApplicationController
   # GET /places/1
   # GET /places/1.json
   def show
+    category = []
+    current_quantity = []
+    Temperature.all.each do |temperature|
+      category << temperature.mearsured_at.strftime("%H:%M")
+      current_quantity << temperature.value
+    end
+
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: '室内温度')
+      f.xAxis(categories: category)
+      f.series(name: '温度', data: current_quantity)
+    end
+
   end
 
   # GET /places/new
