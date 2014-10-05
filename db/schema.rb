0b349e7bc0cd838b4b2ad856b8d1a8b9b5664961
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-10-05 13:26:11 +0900

    Placeを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141004061940) do
+ActiveRecord::Schema.define(version: 20141005042500) do
 
   create_table "lights", force: true do |t|
     t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "places", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
