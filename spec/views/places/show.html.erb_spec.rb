d90eb998071de84a223690d303465d534a6066c8
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-10-09 07:01:17 +0900

    テスト失敗を対処

--- spec/views/places/show.html.erb_spec.rb
+++ spec/views/places/show.html.erb_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe "places/show", :type => :view do
     @place = assign(:place, Place.create!(
       :name => "Name"
     ))
+
+    time = ["44/33"]
+    current_quantity = [0,1,2,3]
+
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: '室内温度')
+      f.xAxis(categories: time)
+      f.series(name: '温度', data: current_quantity)
+    end
   end
 
   it "renders attributes in <p>" do
